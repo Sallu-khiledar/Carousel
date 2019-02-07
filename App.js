040794528a1125593dc09c7341a775f97f67bf60
Sallu-khiledar <47347921+Sallu-khiledar@users.noreply.github.com>
2019-02-07 23:02:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      
+      <div className="App">
+        
+          
+          
+          
+       
+      </div>
+      
+    );
+  }
+  
+}
+
+const styleitem=
+{
+  backgroundcolor:'#f4f4f4',
+  padding:'10px,10px',
+  textAlign:'center'
+}
+
+export default App;
